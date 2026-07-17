5da6b66ce7e06e1232c93688d29a9c573f93609e
Pick5956 <bigmobile5956@gmail.com>
2026-07-17 21:53:34 +0700

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
+  allowedDevOrigins: ["dishy.pro", "www.dishy.pro"],
   images: {
     remotePatterns: [
       {
